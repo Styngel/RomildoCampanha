b583bbf1e3fa83108c75740b70683ea6798e4fd8
Guilherme Nunes <guilhermenunes311@gmail.com>
2024-08-17 22:09:43 -0300

    Resposivo e com menu pronto

--- script.js
+++ script.js
@@ -10,8 +10,16 @@ const temas = [
     document.getElementById("tema9"),
     document.getElementById("tema10")
 ];
+const nav = document.getElementById('navmobile')
 
 const h1 = document.getElementById('principais-propostas')
+
+const todas = [
+    document.getElementById("intro"),
+    document.getElementById("page"),
+]
+
+const imgirmaos = document.getElementById('imgirmaos')
 
 function h1vai(){
     h1.style.display = "none"
@@ -29,15 +37,47 @@ function ocultarTemas() {
 function mostrartema(index) {
     let tema = temas[index];
     
-    if (tema.style.display === "block") {
+    if (tema.style.display === "block"){
         h1vem();
         tema.style.display = "none";
-        
     } 
     else {
         ocultarTemas();
         h1vai();
         tema.style.display = "block"; 
-        
     }
+}
+
+function mostrarmenu(){
+    if (nav.style.display == "block"){
+        nav.style.display = 'none';
+        mostrartodas();
+        ocultarIrmaos();
+    } else{
+        nav.style.display = "block";
+        fechartodas();
+        mostrarIrmaos();
+    }
+}
+
+function fecharnav(){
+    nav.style.display = "none";
+}
+
+function fechartodas(){
+    todas.forEach(todes => {
+        todes.style.display = "none";
+    })
+}
+function mostrartodas(){
+    todas.forEach(todes => {
+        todes.style.display = "block";
+})
+}
+
+function mostrarIrmaos(){
+    imgirmaos.style.display = "block";
+}
+function ocultarIrmaos(){
+    imgirmaos.style.display = "none";
 }
